83c2993a9f7fdf733e2a2b809e1e5a09fb911150
manjeet-devs <74079433+manjeet-devs@users.noreply.github.com>
2025-02-03 07:53:24 +0000

    code updated

--- src/routes/route.jsx
+++ src/routes/route.jsx
@@ -10,14 +10,14 @@ const path = window.location.pathname.replace(/^\//, "");
 console.log("Current path:", path);
 
 const seo = [
-  { route: "/product?product_id=1", url: "new-page" },
-  { route: "/product?product_id=2", url: "new-page1" },
-  { route: "/product?product_id=3", url: "new-page2" },
+  { route: "/product6", url: "new-page" },
+  { route: "/product/4", url: "new-page1" },
+  { route: "/product/3", url: "new-page2" },
 ];
 const seo2 = [
-  { route: "/static?static_id=1", url: "static" },
-  { route: "/static?static_id=2", url: "static1" },
-  { route: "/static?static_id=3", url: "static2" },
+  { route: "/static/3", url: "static4" },
+  { route: "/static/2", url: "static1" },
+  { route: "/static/1", url: "static2" },
 ];
 
 let product_url = null;
@@ -40,8 +40,8 @@ const routes = [
   { path: "/services", element: <ServicesPage /> },
   { path: "/about", element: <AboutPage /> },
   { path: "/contact", element: <ContactPage /> },
-  { path: static_page_url ? `${static_page_url}` : "/static?static_id=:id", element: <StaticPage /> },
-  { path: product_url ? `${product_url}` : "/product?product_id=:id", element: <Product /> }, 
+  { path: static_page_url ? `${static_page_url}` : "/static/:id", element: <StaticPage /> },
+  { path: product_url ? `${product_url}` : "/product/:id", element: <Product /> }, 
   { path: "*", element: <NotFoundPage /> },
 ];
 
