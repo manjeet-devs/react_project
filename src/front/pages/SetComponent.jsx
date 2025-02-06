206ac4338e4b5f88105cf1077f80ed3ba2d22d4f
manjeet-devs <74079433+manjeet-devs@users.noreply.github.com>
2025-02-06 07:24:17 +0000

    code updated

--- src/front/pages/SetComponent.jsx
+++ src/front/pages/SetComponent.jsx
@@ -8,14 +8,14 @@ import { useParams } from "react-router-dom";
 const SetComponent = (getpage) => {
   const { path } = useParams();  
   const seo = [
-    { route: "/product/6", url: "new-page1", file:"product"},
-    { route: "/product/4", url: "new-page2", file:"product"},
-    { route: "/product/3", url: "new-page3", file:"product"},
-    { route: "/static/3", url: "static2", file:"static"},
-    { route: "/static/2", url: "static1", file:"static"},
-    { route: "/category/2", url: "category121", file:"category"},
-    { route: "/category/5", url: "category122", file:"category"},
-    { route: "/category/3", url: "category123", file:"category"},
+    { url: "new-page1", file:"product"},
+    { url: "new-page2", file:"product"},
+    { url: "new-page3", file:"product"},
+    { url: "static2", file:"static"},
+    { url: "static1", file:"static"},
+    { url: "category121", file:"category"},
+    { url: "category122", file:"category"},
+    { url: "category123", file:"category"},
   ];
   let isSeoUrl = null;
   seo.forEach((item) => {
